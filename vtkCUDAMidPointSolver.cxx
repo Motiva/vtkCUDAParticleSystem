01af1f0c6d2123f54abc442a0dee1a0a7929f3f0
Jorge Ballesteros <jballesteros@itccanarias.org>
2011-08-09 15:13:56 +0100

    MidPoint Solver added

--- vtkCUDAMidPointSolver.cxx
+++ vtkCUDAMidPointSolver.cxx
@@ -1,4 +1,4 @@
-#include "vtkCUDAVelocityVerletSolver.h"
+#include "vtkCUDAMidPointSolver.h"
 
 #include "vtkObjectFactory.h"
 #include "vtkDoubleArray.h"
@@ -6,16 +6,16 @@
 #include "vtkCUDAParticleSystem.h"
 #include "vtkCUDAParticleSystem.cuh"
 
-vtkCxxRevisionMacro(vtkCUDAVelocityVerletSolver, "$Revision: 0.1 $");
-vtkStandardNewMacro(vtkCUDAVelocityVerletSolver);
+vtkCxxRevisionMacro(vtkCUDAMidPointSolver, "$Revision: 0.1 $");
+vtkStandardNewMacro(vtkCUDAMidPointSolver);
 
 //----------------------------------------------------------------------------
-vtkCUDAVelocityVerletSolver::vtkCUDAVelocityVerletSolver()
+vtkCUDAMidPointSolver::vtkCUDAMidPointSolver()
 {
 }
 
 //----------------------------------------------------------------------------
-vtkCUDAVelocityVerletSolver::~vtkCUDAVelocityVerletSolver()
+vtkCUDAMidPointSolver::~vtkCUDAMidPointSolver()
 {
 	//Free CUDA arrays
 	freeArray(this->dPos);
@@ -24,7 +24,7 @@ vtkCUDAVelocityVerletSolver::~vtkCUDAVelocityVerletSolver()
 }
 
 //----------------------------------------------------------------------------
-void vtkCUDAVelocityVerletSolver::Init()
+void vtkCUDAMidPointSolver::Init()
 {
 	unsigned int memSize = sizeof(float) * 4 * this->NumberOfParticles;
 
@@ -36,47 +36,60 @@ void vtkCUDAVelocityVerletSolver::Init()
 	allocateArray((void **)&this->dVel, memSize);
 	allocateArray((void **)&this->dAcc, memSize);
 
+	//Allocate derivative vector memory
+	this->p1 = new float[this->NumberOfParticles*4];
+	this->v1 = new float[this->NumberOfParticles*4];
+	memset(this->p1, 0 , memSize);
+	memset(this->v1, 0 , memSize);
+
 }
 
 //----------------------------------------------------------------------------
-void vtkCUDAVelocityVerletSolver::ComputeNextStep(float *p, float *v, float *a)
+void vtkCUDAMidPointSolver::ComputeNextStep(float *p, float *v, float *a)
 {
 	double dt05 = this->DeltaTime*0.5;
 	double dt = this->DeltaTime;
 
-	// Velocity Verlet Equations
-	// * Xn+1 = Xn + Vn*dt + 0.5*An*dt
-	// * Vn+1 = Vn + 0.5*dt(An + An+1)
+	// MidPoint Equations
+	// * Xn+1 = Xn + K2x ; K1x = Vn*dt, K2x = K1x + 0.5*K1v, K1v = An*dt ;
+	// * Xn+1 = Xn + Vn*dt * 0.5*An*dt²
+	// * Vn+1 = Vn + K2v ; K2v = An+0.5 * dt
+
+	unsigned int memSize = sizeof(float) * 4 * this->NumberOfParticles;
+
+	memcpy(p1, p, memSize);
+	memcpy(v1, v, memSize);
 
 	// Copy host -> device
-	unsigned int memSize = sizeof(float) * 4 * this->NumberOfParticles;
 	copyArrayToDevice(this->dPos, p, 0, memSize);
 	copyArrayToDevice(this->dVel, v, 0, memSize);
 	copyArrayToDevice(this->dAcc, a, 0, memSize);
 
-	// CUDA procedure
+	//CUDA procedure
+	//Full Euler step
 	integrateSystem(this->dVel, this->dAcc, dt05, this->NumberOfParticles);
-	integrateSystem(this->dPos, this->dVel, dt, this->NumberOfParticles);
+	integrateSystem(this->dPos, this->dVel, dt05, this->NumberOfParticles);
 
 	// Copy Device -> host
-	copyArrayFromDevice(v, this->dVel, 0, memSize);//Vi = Vn + 0.5*An*dt
-	copyArrayFromDevice(p, this->dPos, 0, memSize);//Xn+1 = Xn + Vn*dt + 0.5*An*dt
+	copyArrayFromDevice(v, this->dVel, 0, memSize);//Vn+0.5 = Vn + 0.5*dt
+	copyArrayFromDevice(p, this->dPos, 0, memSize);//Xn+1 = Xn + Vn*dt + 0.5*An*dt²
 
-	// Derive An+1
+	//midpoint
 	this->DeformationModel->ComputeForces();
 	// Copy host -> device
 	copyArrayToDevice(this->dVel, v, 0, memSize);
 	copyArrayToDevice(this->dAcc, a, 0, memSize);
 
-	integrateSystem(this->dVel, this->dAcc, dt05, this->NumberOfParticles);
+	//Vn+1 = Vn + dt*An+1
+	integrateSystem(this->dVel, this->dAcc, dt, this->NumberOfParticles);
 
 	// Copy Device -> host
-	copyArrayFromDevice(v, this->dVel, 0, memSize);//Vn+1 = Vi + 0.5*dt*An+1 ; Vn+1 = Vn + 1/2*dt*(An + An+1)
+	copyArrayFromDevice(v, this->dVel, 0, memSize);
 
 }
 
 //----------------------------------------------------------------------------
-void vtkCUDAVelocityVerletSolver::PrintSelf(ostream& os, vtkIndent indent)
+void vtkCUDAMidPointSolver::PrintSelf(ostream& os, vtkIndent indent)
 {
 	this->Superclass::PrintSelf(os,indent);
 }
